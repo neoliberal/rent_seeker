d7defdfd9ae81dde22e9498fa5e9d35d8d9e96e7
Abhi Agarwal <goldenercobra@gmail.com>
2018-01-04 12:24:28 -0500

    Add logging

--- rent_seeker/rent_seeker.py
+++ rent_seeker/rent_seeker.py
@@ -1,17 +1,21 @@
 """main class"""
 from typing import List
+import logging
 
 import praw
+from slack_python_logging import slack_logger
 
 class RentSeeker(object):
     """crossposts /new into discussion thread"""
-    __slots__ = ["reddit", "subreddit", "tracked"]
+    __slots__ = ["logger", "reddit", "subreddit", "tracked"]
 
     def __init__(self, reddit: praw.Reddit, subreddit: str) -> None:
         """initialize rentseeker"""
+        self.logger: logging.Logger = slack_logger.initialize("rent_seeker")
         self.reddit: praw.Reddit = reddit
         self.subreddit: praw.models.Subreddit = self.reddit.subreddit(subreddit)
         self.tracked: List[praw.models.Comment] = list()
+        self.logger.info("Successfully initialized")
 
     def listen(self) -> None:
         """listens to subreddit's posts"""
@@ -22,17 +26,20 @@ class RentSeeker(object):
             return int(timegm(datetime.utcnow().utctimetuple()))
 
         start: int = start_time()
+        self.logger.debug("Start time is \"%s\"", start)
         for post in self.subreddit.stream.submissions(pause_after=3):
             if  int(post.created_utc) > start:
                 self.post_comment(post)
 
         for comment in self.tracked:
             if comment.replies:
+                self.logger.debug("Removing found comment replies")
                 moderator: praw.models.reddit.submission.SubmissionModeration = (
                     self._get_discussion_thread().mod
                 )
                 for subcomment in comment.replies:
                     moderator.remove(subcomment)
+                self.logger.debug("Removed comment replies")
         return
 
     def post_comment(self, post: praw.models.Submission) -> None:
@@ -43,12 +50,18 @@ class RentSeeker(object):
             "*Replies to this comment will be removed, please participate in the linked thread*"
         ])
 
+        self.logger.debug("Posting comment")
         comment: praw.models.Comment = discussion_thread.reply(body)
+        self.logger.debug("Posted comment")
+
         self.tracked.append(comment)
         return
 
     def _get_discussion_thread(self) -> praw.models.Submission:
         """returns discussion thread"""
+        self.logger.debug("Finding discussion thread")
         for submission in self.subreddit.search("Discussion Thread", sort="new"):
             if submission.author == self.reddit.user.me():
+                self.logger.debug("Found discussion thread")
                 return submission
+        self.logger.error("Could not find discussion thread")
