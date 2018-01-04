911df082e046f5e0b80c595a8228cf5564f32581
Abhi Agarwal <goldenercobra@gmail.com>
2018-01-04 12:12:25 -0500

    Bot service file

--- rent_seeker/service.py
+++ rent_seeker/service.py
@@ -1,7 +1,28 @@
 """service file"""
+import os
+
+import praw
+
+from .rent_seeker import RentSeeker
 
 def main() -> None:
     """main service function"""
+
+    reddit: praw.Reddit = praw.Reddit(
+        client_id=os.environ["client_id"],
+        client_secret=os.environ["client_secret"],
+        refresh_token=os.environ["refresh_token"],
+        user_agent="linux:rent_seeker:v1.0 (by /u/CactusChocolate)"
+    )
+
+    bot: RentSeeker = RentSeeker(
+        reddit,
+        "neoliberal"
+    )
+
+    while True:
+        bot.listen()
+
     return
 
 if __name__ == "__main__":
