faa856c44a591cbbe421f4809f3b67beb361a02b
Abhi Agarwal <goldenercobra@gmail.com>
2018-01-04 12:16:24 -0500

    Add disclaimer to comment body

--- rent_seeker/rent_seeker.py
+++ rent_seeker/rent_seeker.py
@@ -38,7 +38,10 @@ class RentSeeker(object):
     def post_comment(self, post: praw.models.Submission) -> None:
         """posts comment in discussion thread"""
         discussion_thread: praw.models.Submission = self._get_discussion_thread()
-        body: str = f"New Post in [/new](/r/{self.subreddit}/new): [{post.title}]({post.permalink})"
+        body: str = "\n".join([
+            f"New Post in [/new](/r/{self.subreddit}/new): [{post.title}]({post.permalink})",
+            "*Replies to this comment will be removed, please participate in the linked thread*"
+        ])
 
         comment: praw.models.Comment = discussion_thread.reply(body)
         self.tracked.append(comment)
